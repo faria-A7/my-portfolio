eb64b57733892b6f3e425239abd5dcb8f7669ac0
Faria Akter <149115966+faria-A7@users.noreply.github.com>
2025-08-06 23:57:17 +0600

    Add Site Work,Image, Modify

--- script.js
+++ script.js
@@ -64,3 +64,22 @@ AOS.init({
     duration: 800, // animation duration in ms
     once: true     // only animate once
   });
+
+
+  function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Optional: Close modal if clicking outside the modal content
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+};
