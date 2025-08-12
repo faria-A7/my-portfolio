91cbdfe6e545fdd0a6aad42c287e051363a6e6d4
Faria Akter <149115966+faria-A7@users.noreply.github.com>
2025-08-12 21:29:35 +0600

    Add a new section named Other Research Projects

--- script.js
+++ script.js
@@ -87,3 +87,18 @@ window.onclick = function(event) {
 
 
 
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+window.onclick = function(event) {
+  document.querySelectorAll(".modal").forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
