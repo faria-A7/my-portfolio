6a6cb246a207bd34f96193b112b6b0c453d861e4
Faria Akter <149115966+faria-A7@users.noreply.github.com>
2025-08-22 20:05:56 +0600

    Add navbar features

--- script.js
+++ script.js
@@ -102,3 +102,28 @@ window.onclick = function(event) {
     }
   });
 }
+
+
+
+// ================= Active Navbar Highlight =================
+window.addEventListener("scroll", () => {
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("nav ul li a");
+
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150; // adjust offset for earlier activation
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("nav ul li a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+});
+
